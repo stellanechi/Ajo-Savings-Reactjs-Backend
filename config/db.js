82f2e145ef86f59affb083cca8c9b90a488dab4f
godwin <godwinasogba@gmail.com>
2026-08-18 05:16:52 +0100

    Fix database error logging

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.Erroerror(`r: ${error.message}`);
+        console.error(`Error: ${error.message}`);
         process.exit(1);
     }
 };
